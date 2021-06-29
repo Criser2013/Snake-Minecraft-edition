0d7363de7af44eddda311fa8291de6f5c51a19ba
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-29 10:47:17 -0500

    Arreglos visuales.

--- script-coding/medio/seleccion(medio).js
+++ script-coding/medio/seleccion(medio).js
@@ -25,100 +25,100 @@ function setup() {
 function drawGame () {
     //Esta condición es la encargada de dibujar el menú cuando el "botón 2" se encuentra seleccionado.
     if (Mundo.boton==2) {
-        rect(50,237.5,45,25);
-        textFont('Arial',14)
-        text("Selecciona tu personaje:",110,40)
+        rect(50,277.5,45,25);
+        textFont('Arial',14);
+        text("Selecciona tu personaje:",124,95);
         //Esta pare dibuja los marcos que están por fuera de las imagenes de los personajes.
-        rect(20,100,100,100)
-        rect(150,100,100,100)
-        rect(280,100,100,100)
+        rect(20,140,100,100);
+        rect(150,140,100,100);
+        rect(280,140,100,100);
         //Esta parte dibuja los rectangulos de los botones.
-        rect(52.5,240,40,20)
-        rect(182.5,240,40,20)
-        rect(310,240,40,20)
-        rect(10,300,50,20)
+        rect(52.5,280,40,20);
+        rect(182.5,280,40,20);
+        rect(310,280,40,20);
+        rect(10,30,50,20);
         //Esta parte se encarga de dibujar los textos en pantalla.
-        textFont("Arial",14)
-        text("Steve",55,220)
-        text("Porky",185,220)
-        text("Alex",315,220)
-        text("Jugar",55,255)
-        text("Jugar",185,255)
-        text("Jugar",312.5,255)
-        text("Volver",15,315)
+        textFont("Arial",14);
+        text("Steve",55,260);
+        text("Porky",185,260);
+        text("Alex",315,260);
+        text("Jugar",55,295);
+        text("Jugar",185,295);
+        text("Jugar",312.5,295);
+        text("Volver",15,45);
         //Estas 3 lineas son las encargadas de mostrar las imagenes ya cargadas con la función "preload ()" en pantalla.
-        image(personaje1,20,100,100,100)
-        image(personaje2,150,100,100,100)
-        image(personaje3,280,100,100,100)
+        image(personaje1,20,140,100,100);
+        image(personaje2,150,140,100,100);
+        image(personaje3,280,140,100,100);
     }
     else if (Mundo.boton==3) {
-        rect(180,237.5,45,25);
-        textFont('Arial',14)
-        text("Selecciona tu personaje:",110,40)
-        rect(20,100,100,100)
-        rect(150,100,100,100)
-        rect(280,100,100,100)
-        rect(52.5,240,40,20)
-        rect(182.5,240,40,20)
-        rect(310,240,40,20)
-        rect(10,300,50,20)
-        textFont("Arial",14)
-        text("Steve",55,220)
-        text("Porky",185,220)
-        text("Alex",315,220)
-        text("Jugar",55,255)
-        text("Jugar",185,255)
-        text("Jugar",312.5,255)
-        text("Volver",15,315)
-        image(personaje1,20,100,100,100)
-        image(personaje2,150,100,100,100)
-        image(personaje3,280,100,100,100)
+        rect(180,277.5,45,25);
+        textFont('Arial',14);
+        text("Selecciona tu personaje:",124,95);
+        rect(20,140,100,100);
+        rect(150,140,100,100);
+        rect(280,140,100,100);
+        rect(52.5,280,40,20);
+        rect(182.5,280,40,20);
+        rect(310,280,40,20);
+        rect(10,30,50,20);
+        textFont("Arial",14);
+        text("Steve",55,260);
+        text("Porky",185,260);
+        text("Alex",315,260);
+        text("Jugar",55,295);
+        text("Jugar",185,295);
+        text("Jugar",312.5,295);
+        text("Volver",15,45);
+        image(personaje1,20,140,100,100);
+        image(personaje2,150,140,100,100);
+        image(personaje3,280,140,100,100);
     }
     else if (Mundo.boton==4) {
-        rect(307.5,237.5,45,25);
-        textFont('Arial',14)
-        text("Selecciona tu personaje:",110,40)
-        rect(20,100,100,100)
-        rect(150,100,100,100)
-        rect(280,100,100,100)
-        rect(52.5,240,40,20)
-        rect(182.5,240,40,20)
-        rect(310,240,40,20)
-        rect(10,300,50,20)
-        textFont("Arial",14)
-        text("Steve",55,220)
-        text("Porky",185,220)
-        text("Alex",315,220)
-        text("Jugar",55,255)
-        text("Jugar",185,255)
-        text("Jugar",312.5,255)
-        text("Volver",15,315)
-        image(personaje1,20,100,100,100)
-        image(personaje2,150,100,100,100)
-        image(personaje3,280,100,100,100)
+        rect(307.5,277.5,45,25);
+        textFont('Arial',14);
+        text("Selecciona tu personaje:",124,95);
+        rect(20,140,100,100);
+        rect(150,140,100,100);
+        rect(280,140,100,100);
+        rect(52.5,280,40,20);
+        rect(182.5,280,40,20);
+        rect(310,280,40,20);
+        rect(10,30,50,20);
+        textFont("Arial",14);
+        text("Steve",55,260);
+        text("Porky",185,260);
+        text("Alex",315,260);
+        text("Jugar",55,295);
+        text("Jugar",185,295);
+        text("Jugar",312.5,295);
+        text("Volver",15,45);
+        image(personaje1,20,140,100,100);
+        image(personaje2,150,140,100,100);
+        image(personaje3,280,140,100,100);
     }
     else if (Mundo.boton==1) {
-        rect(7.5,297.5,55,25);
-        textFont('Arial',14)
-        text("Selecciona tu personaje:",110,40)
-        rect(20,100,100,100)
-        rect(150,100,100,100)
-        rect(280,100,100,100)
-        rect(52.5,240,40,20)
-        rect(182.5,240,40,20)
-        rect(310,240,40,20)
-        rect(10,300,50,20)
-        textFont("Arial",14)
-        text("Steve",55,220)
-        text("Porky",185,220)
-        text("Alex",315,220)
-        text("Jugar",55,255)
-        text("Jugar",185,255)
-        text("Jugar",312.5,255)
-        text("Volver",15,315)
-        image(personaje1,20,100,100,100)
-        image(personaje2,150,100,100,100)
-        image(personaje3,280,100,100,100)
+        rect(7.5,27.5,55,25);
+        textFont('Arial',14);
+        text("Selecciona tu personaje:",124,95);
+        rect(20,140,100,100);
+        rect(150,140,100,100);
+        rect(280,140,100,100);
+        rect(52.5,280,40,20);
+        rect(182.5,280,40,20);
+        rect(310,280,40,20);
+        rect(10,30,50,20);
+        textFont("Arial",14);
+        text("Steve",55,260);
+        text("Porky",185,260);
+        text("Alex",315,260);
+        text("Jugar",55,295);
+        text("Jugar",185,295);
+        text("Jugar",312.5,295);
+        text("Volver",15,45);
+        image(personaje1,20,140,100,100);
+        image(personaje2,150,140,100,100);
+        image(personaje3,280,140,100,100);
     }
 }
 function onMouseEvent (Mundo,event) {
