4c4663e1f0f633f9092bc04737d11ce08e178f4c
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-29 16:33:37 -0500

    Ajustes visuales.

--- script-coding/instrucciones.js
+++ script-coding/instrucciones.js
@@ -8,7 +8,7 @@ Contrato: preload variable -> image
 image = Carga una imagen.
 variable = variable local (let)
 Proposito: Carga las imagenes (también funciona con archivos JSON) que se muestran en los menus, antes del "setup ()" para acerlerar 
-la carga de los elementos en pantalla.
+la carga de estos elementos en pantalla.
 Prototipo: preload () {}
 Ejemplos: preload (flecha1,loadImage("images/m1.png")) -> flecha1 = loadImage("images/m1.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
           preload (flecha2,loadImage("images/m2.png")) -> flecha2 = loadImage("images/m2.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
@@ -38,42 +38,42 @@ function drawGame (Mundo) {
             rect(205,360,65,20);
             //Muestra los textos de intrucciones en pantalla.
             textFont('Arial',24);
-            text("Instrucciones fundamentales",50,30);
+            text("Instrucciones fundamentales.",50,30);
             textFont('Arial',20);
             text("Sistema de movimiento:",1,60);
-            text("Apariencias de la serpiente:",1,270);
+            text("Apariencias de la serpiente:",1,260);
             textFont('Arial',14);
             text("Para moverte debes pulsar las teclas:",1,80);
-            text("Importante:\nSi presionas la tecla contraria a la direccion en la que \nse mueve la serpiente LA SERPIENTE NO CAMBIARA DE\nDIRECCIÓN",1,190);
-            text("La apariencia de la serpiente puede ser elegida despues de\nseleccionar la dificultad, si se desea cambiar luego de perder\nen el juego, en la pantalla de puntuación aparecera un botón\npara cambiar la apariencia; la dificultad del juego no se cambia.",1,290);
+            text("Importante:\nSi presionas la tecla contraria a la dirección en la que \nse mueve la serpiente LA SERPIENTE NO CAMBIARÁ DE\nDIRECCIÓN.",1,185);
+            text("La apariencia de la serpiente puede ser elegida después de\nseleccionar el mapa, si se desea cambiar luego de perder\nen el juego, en la pantalla de puntuación aparecera un botón\npara cambiar la apariencia; la dificultad del juego no se verá\nafectada lo mismo aplica para los mapas.",1,280);
             //Muestra los textos de los botones en pantalla.
             text("Volver",132.5,375);
             text("Siguiente",207.5,375);
             //Muestra las imagenes en pantalla.
-            image(flecha1,20,100,75,75);
-            image(flecha2,115,100,75,75);
-            image(flecha3,210,100,75,75);
-            image(flecha4,305,100,75,75);
+            image(flecha1,20,90,75,75);
+            image(flecha2,115,90,75,75);
+            image(flecha3,210,90,75,75);
+            image(flecha4,305,90,75,75);
         }
         else if (Mundo.boton==1) {
             rect(117,357,71,26);
             rect(120,360,65,20);
             rect(205,360,65,20);
             textFont('Arial',24);
-            text("Instrucciones fundamentales",50,30);
+            text("Instrucciones fundamentales.",50,30);
             textFont('Arial',20);
             text("Sistema de movimiento:",1,60);
-            text("Apariencias de la serpiente:",1,270);
+            text("Apariencias de la serpiente:",1,260);
             textFont('Arial',14);
             text("Para moverte debes pulsar las teclas:",1,80);
-            text("Importante:\nSi presionas la tecla contraria a la dirección en la que \nse mueve la serpiente LA SERPIENTE NO CAMBIARA DE\nDIRECCIÓN",1,190);
-            text("La apariencia de la serpiente puede ser elegida despues de\nseleccionar la dificultad, si se desea cambiar luego de perder\nen el juego, en la pantalla de puntuación aparecera un botón\npara cambiar la apariencia; la dificultad del juego no se cambia.",1,290);
+            text("Importante:\nSi presionas la tecla contraria a la dirección en la que \nse mueve la serpiente LA SERPIENTE NO CAMBIARÁ DE\nDIRECCIÓN.",1,185);
+            text("La apariencia de la serpiente puede ser elegida después de\nseleccionar el mapa, si se desea cambiar luego de perder\nen el juego, en la pantalla de puntuación aparecera un botón\npara cambiar la apariencia; la dificultad del juego no se verá\nafectada lo mismo aplica para los mapas.",1,280);
             text("Volver",132.5,375);
             text("Siguiente",207.5,375)
-            image(flecha1,20,100,75,75);
-            image(flecha2,115,100,75,75);
-            image(flecha3,210,100,75,75);
-            image(flecha4,305,100,75,75);
+            image(flecha1,20,90,75,75);
+            image(flecha2,115,90,75,75);
+            image(flecha3,210,90,75,75);
+            image(flecha4,305,90,75,75);
         }
     }
     else if (Mundo.menu==2) {
@@ -82,11 +82,11 @@ function drawGame (Mundo) {
             rect(120,360,65,20);
             rect(205,360,65,20);
             textFont("Arial",20);
-            text("Acerca de las dificultades",90,30);
+            text("Acerca de las dificultades.",90,30);
             textFont("Arial",14);
-            text("- Facil: En esta dificultad, la velocidad de movimiento es lenta,\nla única forma de perder es tomando alguna trampa o chocan-\ndo la cabeza de la serpiente con su cola!",2,60)
-            text("- Medio: En esta dificultad, la velocidad de movimiento se ve\nlevemente incrementada respecto a la dificultad anterior, las\ntrampas y powerups aparecen con mas frecuencia y solo los\nlaterales del mapa son traspasables, siendo ahora lo limites\nsuperiores no traspasables y si chocas con ellos pierdes!",2,120)
-            text("- Dificil: En esta dificultad, la velocidad de movimiento es nota-\nblemente mas rapida respecto a las anteriores, ninguna parte\ndel mapa es traspasable y las trampas aparecen con mucha\nmayor frecuencia.",2,215)
+            text("- Fácil: En esta dificultad, la velocidad de movimiento es lenta,\nla única forma de perder es tomando alguna manzana de oro, \nchocando la cabeza de la serpiente con su cola o con algún\nenemigo.",2,60)
+            text("- Medio: En esta dificultad, la velocidad de movimiento se ve\nlevemente incrementada respecto a la dificultad anterior, las\nmanzanas de oro y enemigos aparecen con mas frecuencia y\nsolo los laterales del mapa son traspasables, siendo ahora los\nlímites superiores no traspasables y ¡si chocas con ellos pi-\nerdes!",2,135)
+            text("- Difícil: En esta dificultad, la velocidad de movimiento es nota-\nblemente mas rápida respecto a las anteriores, ninguna parte\ndel mapa es traspasable y los enemigos, manzanas de oro\naparecen con mucha mayor frecuencia.",2,245)
             text("Volver",132.5,375);
             text("Siguiente",207.5,375);
         }
@@ -95,11 +95,11 @@ function drawGame (Mundo) {
             rect(120,360,65,20);
             rect(205,360,65,20);
             textFont("Arial",20);
-            text("Acerca de las dificultades",90,30);
+            text("Acerca de las dificultades.",90,30);
             textFont("Arial",14);
-            text("- Facil: En esta dificultad, la velocidad de movimiento es lenta,\nla única forma de perder es tomando alguna trampa o chocan-\ndo la cabeza de la serpiente con su cola!",2,60)
-            text("- Medio: En esta dificultad, la velocidad de movimiento se ve\nlevemente incrementada respecto a la dificultad anterior, las\ntrampas y powerups aparecen con mas frecuencia y solo los\nlaterales del mapa son traspasables, siendo ahora lo limites\nsuperiores no traspasables y si chocas con ellos pierdes!",2,120)
-            text("- Dificil: En esta dificultad, la velocidad de movimiento es nota-\nblemente mas rapida respecto a las anteriores, ninguna parte\ndel mapa es traspasable y las trampas aparecen con mucha\nmayor frecuencia.",2,215)
+            text("- Fácil: En esta dificultad, la velocidad de movimiento es lenta,\nla única forma de perder es tomando alguna manzana de oro, \nchocando la cabeza de la serpiente con su cola o con algún\nenemigo.",2,60)
+            text("- Medio: En esta dificultad, la velocidad de movimiento se ve\nlevemente incrementada respecto a la dificultad anterior, las\nmanzanas de oro y enemigos aparecen con mas frecuencia y\nsolo los laterales del mapa son traspasables, siendo ahora los\nlímites superiores no traspasables y ¡si chocas con ellos pi-\nerdes!",2,135)
+            text("- Difícil: En esta dificultad, la velocidad de movimiento es nota-\nblemente mas rápida respecto a las anteriores, ninguna parte\ndel mapa es traspasable y los enemigos, manzanas de oro\naparecen con mucha mayor frecuencia.",2,245)
             text("Volver",132.5,375);
             text("Siguiente",207.5,375);
         }
@@ -108,12 +108,12 @@ function drawGame (Mundo) {
         rect(161.5,357,71,26);
         rect(164.5,360,65,20);
         textFont("Arial",20);
-        text("Elementos dentro del juego",85,30);
+        text("Elementos dentro del juego.",85,30);
         textFont("Arial",14);
-        text("- Comida: La comida ayuda a crecer a la serpiente, cada que\ntu snake come una fruta tu puntuación aumentará en 1 punto.",3,60);
-        text("- Powerup de velocidad: Es un powerup con apariencia azul\nque aparece a partir de tener una puntuación superior a 4, al\ntomarlo la velocidad del juego aumentará 100% - 33% durante\nun breve periodo de tiempo. Al tomarla las trampas no reapa-\nreceran.",3,100);
-        text("- Fruta trampa: Posee la misma apariencia que la comida y el\npowerup, es de color negro y al tomarla ¡automaticamente pier-\ndes!, aparece a partir de los 10 puntos, en caso de no tomarla\nreaparece en un rango de tiempo de 20 segundos, reaparece\nen un rango de 4 casillas en los ejes vertical y horizontal, asi\nque ¡cuidado!",3,190);
-        text("- Obstaculos: Aparecen a partir de los 10 puntos, siguen la\nmisma mecanica de la fruta trampa asi que ¡cuidado por don-\nde te mueves!",3,300)
+        text("- Manzanas: Las manzanas ayuda a crecer a la serpiente, ca-\nda que tu snake come una manzana tu puntuación aumentará\nen 1 punto y crecerá.",3,60);
+        text("- Manzana de oro: Es una manzana de color dorado que\naparece a partir de tener una puntuación superior a 4, al\ntomarlo la velocidad del juego aumentará 100% - 33% durante\nun breve período de tiempo. Al tomarla los enemigos no reapa-\nreceran. Estas NO AUMENTAN TU PUNTUACIÓN.",3,115);
+        text("- Objetos perjudiciales: Son elementos que su apariencia varía\ndependiendo del mapa, al tomarla ¡automaticamente pierdes!,\naparece a partir de los 10 puntos, en caso de no tomarla\nreaparece en un rango de tiempo de 20 segundos, reaparece\nen un radio de 4 casillas en los ejes vertical y horizontal, asi\nque ¡cuidado!",3,205);
+        text("- Enemigos: Aparecen a partir de los 10 puntos, siguen la\nmisma mecanica de la fruta trampa asi que ¡cuidado por don-\nde te mueves!",3,315)
         text("Volver",176.5,375);
     }
 }
