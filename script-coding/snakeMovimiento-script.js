8470577086efc63b3eac130f79b88c97c97ce1f2
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-29 09:02:56 -0500

    Trampa de velocidad añadida.
    Se añadió una trampa de velocidad que aparece aleatoriamente en todo el mapa, se documentaron la funciones y condicionales. Se puso como aleatorio el spawn de comida y las trampas al inicio del juego.

--- script-coding/snakeMovimiento-script.js
+++ script-coding/snakeMovimiento-script.js
@@ -4,16 +4,16 @@
 let { append, cons, first, isEmpty, isList, length, rest, map, forEach }  = functionalLight;
 debugger;
 /*
-Contrato: posx,posy () -> number
+Contrato: foodposx,foodposy () -> number
 Proposito: Determina la posición en "X" (posx) y "Y" (posy) de la comida del snake de forma aleatoria dentro del mapa.
-Prototipo: posx,posy () {}
-Ejemplos: posx,posy () -> {x:4,y:7}
-          posx,posy () -> {x:1,y;1}
+Prototipo: foodposx,foodposy () {}
+Ejemplos: foodposx,foodposy () -> {x:4,y:7}
+          foodposx,foodposy () -> {x:1,y;1}
 */
-const x1 = function posx () {
+function foodposx () {
   return Math.ceil(Math.random() * (20 - 1)) + 1;
 };
-const y1 = function posy () {
+function foodposy () {
   return Math.ceil(Math.random() * (20 - 1)) + 1;
 };
 /*
@@ -47,7 +47,6 @@ function moveSnake(snake, dir) {
 //Estas constantes definen el ancho y alto de los elementos de snake.
 const dx = 20;
 const dy = 20;
-
 /**
  * Esto se llama antes de iniciar el juego
  */
@@ -55,15 +54,18 @@ function setup() {
   frameRate(5);
   createCanvas(400, 400);
   background(15, 200, 50);
-  Mundo = {snake: [{x:3,y:1}, {x:2,y:1}, {x:1,y:1 }],dir:{x:1,y:0},food:{x:Math.ceil(Math.random()*(20-1))+1, y:Math.ceil(Math.random()*(20-1))+1},score:0,colision:false}
+  Mundo = {snake: [{x:3,y:1}, {x:2,y:1}, {x:1,y:1 }],dir:{x:1,y:0},food:{x:Math.ceil(Math.random()*(20-1))+1, y:Math.ceil(Math.random()*(20-1))+1},score:0,colision:false,trampas:{x:Math.ceil(Math.random()*(20-1))+1,y:Math.ceil(Math.random()*(20-1))+1,estado:false},contador:0}
 }
 // Dibuja algo en el canvas. Aqui se pone todo lo que quieras pintar.
 function drawGame(Mundo){
+  if (Mundo.score>=5&&Mundo.trampas.estado==false) {
   background(10, 200, 50);
   //Esta linea llama a la función drawFood para dibujar la comida.
   drawFood(Mundo.food);
   //Esta linea llama a la función drawScore para dibujar el puntaje.
   drawScore(Mundo.score);
+  //Estas linea llama a la función drawCheat para dibujar las trampas.
+  drawCheat(Mundo.trampas);
   fill(240, 240, 240);
   //Esta función se encarga de dibujar cada elemento del snake, incluida la cabeza de la serpiente, todo se dibuja con la misma apariencia y características.
   forEach(Mundo.snake, s => {
@@ -81,6 +83,27 @@ function drawGame(Mundo){
   rect((first(Mundo.snake).x*dx)+8.2,(first(Mundo.snake).y*dy)+8,3.4,6);
   rect((first(Mundo.snake).x*dx)+6.2,(first(Mundo.snake).y*dy)+11.12,1.7,6);
   rect((first(Mundo.snake).x*dx)+11.6,(first(Mundo.snake).y*dy)+11.12,1.7,6);
+  }
+  else {
+  background(10, 200, 50);
+  drawFood(Mundo.food);
+  drawScore(Mundo.score);
+  fill(240, 240, 240);
+  forEach(Mundo.snake, s => {
+    rect(s.x * dx, s.y * dy, dx, dy);
+  });
+  stroke (0);
+  fill(13,181,13);
+  rect(first(Mundo.snake).x*dx,first(Mundo.snake).y*dy,dx,dy);
+  fill (0);
+  stroke (0);
+  rect((first(Mundo.snake).x*dx)+3.2,(first(Mundo.snake).y*dy)+4,5,5);
+  rect((first(Mundo.snake).x*dx)+12,(first(Mundo.snake).y*dy)+4,5,5);
+  fill(1);
+  rect((first(Mundo.snake).x*dx)+8.2,(first(Mundo.snake).y*dy)+8,3.4,6);
+  rect((first(Mundo.snake).x*dx)+6.2,(first(Mundo.snake).y*dy)+11.12,1.7,6);
+  rect((first(Mundo.snake).x*dx)+11.6,(first(Mundo.snake).y*dy)+11.12,1.7,6);
+}
 }
 /*
 Contrato: drawFood coordenadas -> food
@@ -108,13 +131,39 @@ function drawScore (score) {
   text("Score: "+score,10,380);
 }
 /*
-Contrato: colision list -> boolean 
-Proposito: Determina si hubo colisión entre la cabeza del snake y alguna parte de su cuerpo (representado en forma de lista).
-Prototipo: colision (lista) {}
-Ejemplos: colision ([{x:1,y:1},{x:2,y:1},{x:1,y:1}]) -> true
-          colision ([{x:3,y:1},{x:2,y:1},{x:1,y:1}]) -> false
+Contrato: cheatposx,cheatposy () -> number
+Proposito: Determina la posición en "X" (posx) y "Y" (posy) de la las trampas de forma aleatoria dentro del mapa.
+Prototipo: cheatposx,cheatposy () {}
+Ejemplos: cheatposx,cheatposy () -> {x:4,y:7}
+          cheatposx,cheatposy () -> {x:1,y;1}
 */
-function colision (lista) {
+function cheatposx () {
+  return Math.ceil(Math.random() * (20 - 1)) + 1;
+};
+function cheatposy () {
+  return Math.ceil(Math.random() * (20 - 1)) + 1;
+};
+/*
+Contrato: drawCheat coordenadas -> cheat
+coordenadas = puntos en los ejes "X" y "Y" generados al azar en el intervalo 1 - 20 por la variable "Mundo".
+cheat = coordenadas en los ejes "X" y "Y", junto a las dimensiones de un circulo para dibujar las trampas dentro el juego.
+Proposito: Determina la posición en "X" (posx) y "Y" (posy) de la comida del snake de forma aleatoria dentro del mapa.
+Prototipo: foodposx,foodposy () {}
+Ejemplos: drawCheat (4,7) -> ellipse (3,4,20,20)
+          drawCheat (1,1) -> ellipse (3,4,20,20)
+*/
+function drawCheat (trampa) {
+  fill(0,0,255);
+  ellipse((trampa.x*dx)-10,(trampa.y*dy)-10,dx,dy);
+}
+/*
+Contrato: colisionCabeza list -> boolean 
+Proposito: Determina si hubo colisión entre la cabeza del snake y alguna parte de su cuerpo (representado en forma de lista).
+Prototipo: colisionCabeza (lista) {}
+Ejemplos: colisionCabeza ([{x:1,y:1},{x:2,y:1},{x:1,y:1}]) -> true
+          colisionCabeza ([{x:3,y:1},{x:2,y:1},{x:1,y:1}]) -> false
+*/
+function colisionCabeza (lista) {
   const kbeza = first(Mundo.snake)
   if (isEmpty(rest(lista))) {
     return false;
@@ -122,8 +171,11 @@ function colision (lista) {
   else if ((first(rest(lista)).x==kbeza.x)&&(first(rest(lista)).y==kbeza.y)) {
     return true;
   }
+  else if ((first(lista).x==first(rest(lista)).x)&&(first(lista).y==first(rest(lista)).y)) {
+    return true;
+  }
   else {
-    return colision(rest(lista));
+    return colisionCabeza(rest(lista));
   }
 }
 /*
@@ -150,17 +202,17 @@ function traslacion (lista) {
   }
 }
 /*
-Contrato: colisionp Mundo -> boolean
+Contrato: colisionparedes Mundo -> boolean
 Mundo = Posicion del snake, la comida, el puntaje, colisiones, etc.
 Proposito: Determina si hubo colision de la cabeza de la serpiente con el límite inferior o superior del mapa, usando la posición de la cabeza del Snake
 y la dirección en la que se mueven, SOLO TOMA LA DIRECCIÓN EN EL EJE "Y". Si hubo colisión actualiza el "Mundo" en la parte de colisiones por este valor booleano.
-Prototipo: colisionp () {}
-Ejemplos: colisionp ({snake:[{x:1,y:19},{x:2,y:18},{x:1,y:17}],dir:{x:0,y:1}) -> true
-          colisionp ({snake:[{x:1,y:0},{x:1,y:1},{x:1,y:2}],dir:{x:0,y:-1}) -> true
-          colisionp ({snake:[{x:1,y:18},{x:1,y:17},{x:1,y:16}],dir:{x:0,y:1}) -> false
-          colisionp ({snake:[{x:1,y:1},{x:1,y:2},{x:1,y:3}],dir:{x:0,y:-1}) -> false
+Prototipo: colisionparedes () {}
+Ejemplos: colisionparedes ({snake:[{x:1,y:19},{x:2,y:18},{x:1,y:17}],dir:{x:0,y:1}) -> true
+          colisionparedes ({snake:[{x:1,y:0},{x:1,y:1},{x:1,y:2}],dir:{x:0,y:-1}) -> true
+          colisionparedes ({snake:[{x:1,y:18},{x:1,y:17},{x:1,y:16}],dir:{x:0,y:1}) -> false
+          colisionparedes ({snake:[{x:1,y:1},{x:1,y:2},{x:1,y:3}],dir:{x:0,y:-1}) -> false
 */
-function colisionp () {
+function colisionparedes () {
   if ((((first(Mundo.snake).y<=0)&&(Mundo.dir.y==-1))||(first(Mundo.snake).y>=19)&&(Mundo.dir.y==1))&&Mundo.colision==false) {
     return true
   }
@@ -169,6 +221,35 @@ function colisionp () {
   }
   else {
     return false
+  }
+}
+/*
+Contrato: fpscheat () -> number
+Proposito: Determina la velocidad de refresco de los fotogramas en función del puntaje (actua como funcion auxiliar de la función "onTic" dentro de
+los condicionales dedicados al spawn de las trampas y sus efectos).
+Prototipo: fpscheat () {}
+Ejemplos: fpscheat (0) -> 5
+          fpscheat (10) -> 7.5
+          fpscheat (20) -> 10
+          fpscheat (30) -> 12.5
+          fpscheat (50) -> 15
+          fpscheat (69) -> 15
+*/
+function fpscheat () {
+  if (Mundo.score>=0&&Mundo.score<10) {
+    return 5
+  }
+  else if (Mundo.score>=10&&Mundo.score<20) {
+    return 7.5
+  }
+  else if (Mundo.score>=20&&Mundo.score<30) {
+    return 10
+  }
+  else if (Mundo.score>=30&&Mundo.score<50) {
+    return 12.5
+  }
+  else if (Mundo.score>=50) {
+    return 15
   }
 }
 /*
@@ -189,38 +270,63 @@ function mousePressed () {
 // Esto se ejecuta en cada tic del reloj. Con esto se pueden hacer animaciones
 function onTic(Mundo){
   //Si la funcion colisionp determina que si hubo colisión (retornando un "true"), esto se ejecuta para dibujar el botón de reinicio y mostrar el puntaje alcanzado.
-  if (colisionp(Mundo.snake)==true) {
-        textFont("Arial",16);
-        text("Haz perdido, tu puntuaci\xf3n es: "+Mundo.score,90,200);
-        fill(255);
-        rect(145,220,110,20);
-        fill(1);
-        textFont("Arial",14);
-        text("Jugar de nuevo",150,235);
-        return update(Mundo,{colision:true});
+  if (colisionparedes(Mundo.snake)==true) {
+    textFont("Arial",16);
+    text("Haz perdido, tu puntuaci\xf3n es: "+Mundo.score,90,200);
+    fill(255);
+    rect(145,220,110,20);
+    fill(1);
+    textFont("Arial",14);
+    text("Jugar de nuevo",150,235);
+    return update(Mundo,{colision:true});
+  }
+  //Realiza lo mismo que la condición anterior, solo que ahora es con la colisión de la cabeza del snake con alguna parte de su cuerpo.
+  else if (colisionCabeza(Mundo.snake)==true) {
+    textFont("Arial",16);
+    text("Haz perdido, tu puntuaci\xf3n es: "+Mundo.score,90,200);
+    fill(255);
+    rect(145,220,110,20);
+    fill(1);
+    textFont("Arial",14);
+    text("Jugar de nuevo",150,235);
+    return update(Mundo,{colision:true});
   }
   /* Esta condición actualiza la dirección en "X" del en el Mundo, si la serpiente se mueve hacia la derecha y se presiona la tecla para mover a la 
   izquierda, para que permanezca moviendose hacia la derecha. */
-  else if (((first(Mundo.snake).x>first(rest(Mundo.snake)).x)&&Mundo.dir.x==-1)&&colision(Mundo.snake)==false) {
+  else if (((first(Mundo.snake).x>first(rest(Mundo.snake)).x)&&Mundo.dir.x==-1)&&colisionCabeza(Mundo.snake)==false) {
     return update(Mundo,{snake: moveSnake(Mundo.snake,{x:1,y:0}),dir:{x:1,y:0}});
   }
   //Realiza la misma labor que la condición anterior solo que en la dirección contaria y presionando la tecla contraria.
-  else if (((first(Mundo.snake).x<first(rest(Mundo.snake)).x)&&Mundo.dir.x==1)&&colision(Mundo.snake)==false) {
+  else if (((first(Mundo.snake).x<first(rest(Mundo.snake)).x)&&Mundo.dir.x==1)&&colisionCabeza(Mundo.snake)==false) {
     return update(Mundo,{snake: moveSnake(Mundo.snake,{x:-1,y:0}),dir:{x:-1,y:0}})
   }
   //Realiza la misma labor que las condiciones anteriores solo que en el eje "Y".
-  else if (((first(Mundo.snake).y>first(rest(Mundo.snake)).y)&&Mundo.dir.y==-1)&&colision(Mundo.snake)==false) {
+  else if (((first(Mundo.snake).y>first(rest(Mundo.snake)).y)&&Mundo.dir.y==-1)&&colisionCabeza(Mundo.snake)==false) {
     return update(Mundo,{snake: moveSnake(Mundo.snake,{x:0,y:1}),dir:{x:0,y:1}})
   }
-  else if (((first(Mundo.snake).y<first(rest(Mundo.snake)).y)&&Mundo.dir.y==1)&&colision(Mundo.snake)==false) {
+  else if (((first(Mundo.snake).y<first(rest(Mundo.snake)).y)&&Mundo.dir.y==1)&&colisionCabeza(Mundo.snake)==false) {
     return update(Mundo,{snake: moveSnake(Mundo.snake,{x:0,y:-1}),dir:{x:0,y:-1}})
   }
   //Estas 2 condiciones determinan si hubo colisión entre la cabeza del snake y la comida.
   else if ((first(Mundo.snake).x+1==Mundo.food.x)&&(first(Mundo.snake).y+1==Mundo.food.y)) {
-    return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:{x:x1(),y:y1()},score:Mundo.score+1})
+  return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:{x:foodposx(),y:foodposy()},score:Mundo.score+1})
   }
   else if (((first(Mundo.snake).x==Mundo.food.x)&&(first(Mundo.snake).y+1==Mundo.food.y))&&((Mundo.dir.y!==1)&&(Mundo.dir.x!==0))) {
-    return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:{x:x1(),y:y1()},score:Mundo.score+1})
+    return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:{x:foodposx(),y:foodposy()},score:Mundo.score+1})
+  }
+   //Estas 2 condiciones determinan si hubo colisión entre la cabeza del snake y una trampa, aparte de habilitar el efecto de aumento de velocidad, aumentando los FPS del juego 5.
+  else if (((first(Mundo.snake).x+1==Mundo.trampas.x)&&(first(Mundo.snake).y+1==Mundo.trampas.y))&&Mundo.score>=5) {
+    frameRate(fpscheat()+5)
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:{estado:true},contador:Mundo.contador+1})
+  }
+  else if ((((first(Mundo.snake).x==Mundo.trampas.x)&&(first(Mundo.snake).y+1==Mundo.trampas.y))&&((Mundo.dir.y!==1)&&(Mundo.dir.x!==0)))&&Mundo.score>=5) {
+    frameRate(fpscheat()+5)
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:{estado:true},contador:Mundo.contador+1})
+  }
+   //Esta condición es la encargada de hacer desaparecer el efecto de aumento de velocidad en la serpiente luego de 20 segundos 
+  else if (Mundo.contador==80&&Mundo.trampas.estado==true) {
+    frameRate(fpscheat())
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:{x:cheatposx(),y:cheatposy(),estado:false},contador:0})
   }
   //Estas condiciones determinan si la cabeza del snake se encuentra en los límites laterales del mapa, para asi realizar el cambio de posición.
   else if (first(Mundo.snake).x==20) {
@@ -229,39 +335,40 @@ function onTic(Mundo){
   else if (first(Mundo.snake).x==-1) {
     return update(Mundo,{snake:moveSnake(traslacion(Mundo.snake),Mundo.dir)})
   }
-  //Aumenta la velocidad del juego en función del puntaje.
-  else if (Mundo.score>=10&&Mundo.score<20) {
-    frameRate(7.5);
-    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)});
+   //Estas 2 condiciones determinan si hubo colisión entre la cabeza del snake y la comida.
+  else if (Mundo.trampas.estado==true&&Mundo.contador<80) {
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1})
   }
-  else if (Mundo.score>=20&&Mundo.score<30) {
+   //Esta condición actua como cronometro para spawnear una nueva trampa siempre y cuando el usuario no la haya cogido.
+  else if ((Mundo.contador>=0&&Mundo.contador<40)&&Mundo.trampas.estado==false) {
+    frameRate(fpscheat());
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1})
+  }
+   //Esta condicion spawnea una nueva trampa cada que la condicion anterior alcanza un valor de 40 en el parametro "contador" del mundo.
+  else if (Mundo.contador==40&&Mundo.trampas.estado==false) {
+    frameRate(fpscheat());
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:{x:cheatposx(),y:cheatposy(),estado:false},contador:0})
+  }
+  //Aumenta la velocidad del juego en función del puntaje.
+  else if (Mundo.score>=10&&Mundo.score<20&&(Mundo.trampas.estado==false&&Mundo.contador==0)) {
+    frameRate(7.5);
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)})
+  }
+  else if (Mundo.score>=20&&Mundo.score<30&&(Mundo.trampas.estado==false&&Mundo.contador==0)) {
     frameRate(10);
     return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)});
   }
-  else if (Mundo.score>=30&&Mundo.score<40) {
+  else if (Mundo.score>=30&&Mundo.score<50&&(Mundo.trampas.estado==false&&Mundo.contador==0)) {
     frameRate(12.5);
     return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)});
   }
-  else if (Mundo.score>=50) {
+  else if (Mundo.score>=50&&(Mundo.trampas.estado==false&&Mundo.contador==0)) {
     frameRate(15);
     return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)});
   }
   else {
-    //Realiza la mismo que la primera condición de "onTic", solo que ahora es con la colisión de la cabeza del snake con alguna parte de su cuerpo.
-    if (colision(Mundo.snake)==true) {
-      textFont("Arial",16);
-      text("Haz perdido, tu puntuaci\xf3n es: "+Mundo.score,90,200);
-      fill(255);
-      rect(145,220,110,20);
-      fill(1);
-      textFont("Arial",14);
-      text("Jugar de nuevo",150,235);
-      return update(Mundo,{colision:true});
-    }
     //Actualiza la posición del snake usando la función "moveSnake".
-    else {
-        return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)})
-    }
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir)})
   }
 }
 //Implemente esta función si quiere que su programa reaccione a eventos del mouse
