b80adddc1985aa4a83b8994bf41f4db47420f6fa
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-30 07:53:28 -0500

    Decoración pantalla de game over.
    Se decoró la pantalla de game over colocando un fondo y empleando la fuente subida con anterioridad.

--- script-coding/medio/perdido(medio-alex-overworld).js
+++ script-coding/medio/perdido(medio-alex-overworld).js
@@ -1,6 +1,24 @@
 let Mundo = {}
+let fuente = null;
+let fondo = null;
+/*
+Contrato: preload variable -> FoI
+FoI = Font or image.
+Image = Imagen dentro de los archivos del juego.
+Font = Fuente tipográfica no incluida en CSS ni P5.JS.
+variable = variable local (let)
+Proposito: Carga la fuente estilo Minecraft que utiliza este menú del juego y la imagen de fondo.
+Prototipo: preload () {}
+Ejemplos: preload (fuente,loadImage("minecraft.otf")) -> fuente = loadFont("minecraft.otf") // La fuente se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
+          preload (fondo,loadImage("images/m2.png")) -> fondo = loadImage("images/m2.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
+*/
+function preload () {
+    fuente = loadFont("minecraft.otf");
+    fondo = loadImage("images/fondo_perdido.png");
+}
 function setup () {
     createCanvas(400,400);
+    background(fondo);
     Mundo = {boton:1}
 }
 function update(data, attribute) {
@@ -9,86 +27,94 @@ function update(data, attribute) {
 function drawGame (Mundo) {
     //Cuando un botón está seleccionado todos dibujan los mismo a excepción del rectangulo que está alrededor del botón que se encuentra seleccionado.
     if (Mundo.boton==1) {
+        fill(109,109,109);
+        //Esta parte dibuja el botón que se encuentra seleccionado (el 1º).
+        stroke(255)
+        rect(95,220,209,20);
+        //Esta parte dibuja los botones que no se encuentran seleccionados.
+        stroke(1)
+        rect(95,260,209,20);
+        rect(95,300,209,20);
+        rect(95,340,209,20);
+        noStroke();
         fill(255);
-        //Dibuja un rectangulo alrededor del botón para indicar que ese es el selccionado.
-        rect(142,217,116,26);
-        //Estas 4 líneas dibujan los botones.
-        rect(145,220,110,20);
-        rect(110,260,177,20);
-        rect(125,300,147,20);
-        rect(140,340,120,20);
-        fill(1);
         //Esta parte dibuja los textos en pantalla.
-        textFont("Arial",24);
-        text("Juego terminado.",110,60);
-        textFont("Arial",18);
-        text("Empieza una nueva partida, manteniendo",35,122.5);
-        text("la misma dificultad, mapa y personaje",47.5,142.5);
-        text("seleccionados previamente.",87.5,162.5);
-        textFont("Arial",14);
-        text("Jugar de nuevo",150,235);
-        text("Seleccionar otro personaje",115,275);
-        text("Seleccionar otro mapa",127.5,315);
-        text("Cambiar dificultad",142.5,355);
+        textFont(fuente,24);
+        text("Juego terminado.",100,60);
+        textFont(fuente,18);
+        text("Empieza una nueva partida, manteniendo",22,130);
+        text("la misma dificultad, mapa y personaje",32.5,150);
+        text("seleccionados previamente.",77,170);
+        textFont(fuente,14);
+        text("Jugar de nuevo",145,235);
+        text("Seleccionar otro personaje",100,275);
+        text("Seleccionar otro mapa",120,315);
+        text("Cambiar dificultad",137.5,355);
     }
     else if (Mundo.boton==2) {
+        fill(109,109,109);
+        stroke(255);
+        rect(95,260,209,20);
+        stroke(1);
+        rect(95,220,209,20);
+        rect(95,300,209,20);
+        rect(95,340,209,20);
+        noStroke();
         fill(255);
-        rect(107,257,183,26);
-        rect(145,220,110,20);
-        rect(110,260,177,20);
-        rect(125,300,147,20);
-        rect(140,340,120,20);
-        fill(1);
-        textFont("Arial",24);
-        text("Juego terminado.",110,60);
-        textFont("Arial",18);
-        text("Vuelve al menú de selección de personajes",27.5,122.5);
-        text("y empieza una nueva partida manteniendo",32.5,142.5);
-        text("la dificultad y el mapa seleccionado.",60,162.5);
-        textFont("Arial",14);
-        text("Jugar de nuevo",150,235);
-        text("Seleccionar otro personaje",115,275);
-        text("Seleccionar otro mapa",127.5,315);
-        text("Cambiar dificultad",142.5,355);
+        textFont(fuente,24);
+        text("Juego terminado.",100,60);
+        textFont(fuente,18);
+        text("Vuelve al menú de selección de persona-",10,130);
+        text("jes y empieza una nueva partida manteni-",10,150);
+        text("endo la dificultad y el mapa seleccionado.",14,170);
+        textFont(fuente,14);
+        text("Jugar de nuevo",145,235);
+        text("Seleccionar otro personaje",100,275);
+        text("Seleccionar otro mapa",120,315);
+        text("Cambiar dificultad",137.5,355);
     }
     else if (Mundo.boton==3) {
+        fill(109,109,109);
+        stroke(255);
+        rect(95,300,209,20);
+        stroke(1);
+        rect(95,220,209,20);
+        rect(95,260,209,20);
+        rect(95,340,209,20);
+        noStroke();
         fill(255);
-        rect(122,297,153,26);
-        rect(145,220,110,20);
-        rect(110,260,177,20);
-        rect(125,300,147,20);
-        rect(140,340,120,20);
-        fill(1);
-        textFont("Arial",24);
-        text("Juego terminado.",110,60);
-        textFont("Arial",18);
-        text("Cambia el mapa del juego, la dificultad no se",20,137.5);
-        text("verá afectada, deberás seleccionar",57.5,157.5);
-        text("nuevamente tu personaje.",97.5,177.5);
-        textFont("Arial",14);
-        text("Jugar de nuevo",150,235);
-        text("Seleccionar otro personaje",115,275);
-        text("Seleccionar otro mapa",127.5,315);
-        text("Cambiar dificultad",142.5,355)
+        textFont(fuente,24);
+        text("Juego terminado.",100,60);
+        textFont(fuente,18);
+        text("Cambia el mapa del juego, la dificultad no",15,130);
+        text("se verá afectada, deberás seleccionar",20,150);
+        text("nuevamente tu personaje.",82.5,170);
+        textFont(fuente,14);
+        text("Jugar de nuevo",145,235);
+        text("Seleccionar otro personaje",100,275);
+        text("Seleccionar otro mapa",120,315);
+        text("Cambiar dificultad",137.5,355);
     }
     else if (Mundo.boton==4) {
+        fill(109,109,109);
+        stroke(255);
+        rect(95,340,209,20);
+        stroke(1);
+        rect(95,220,209,20);
+        rect(95,260,209,20);
+        rect(95,300,209,20);
+        noStroke();
         fill(255);
-        rect(137,337,126,26);
-        rect(145,220,110,20);
-        rect(110,260,177,20);
-        rect(125,300,147,20);
-        rect(140,340,120,20);
-        fill(1);
-        textFont("Arial",24);
-        text("Juego terminado.",110,60);
-        textFont("Arial",18);
-        text("Cambia la dificultad del juego, tendrás que",27.5,137.5);
-        text("seleccionar nuevamente tu personaje.",45,157.5);
-        textFont("Arial",14);
-        text("Jugar de nuevo",150,235);
-        text("Seleccionar otro personaje",115,275);
-        text("Seleccionar otro mapa",127.5,315);
-        text("Cambiar dificultad",142.5,355);
+        textFont(fuente,24);
+        text("Juego terminado.",100,60);
+        textFont(fuente,18);
+        text("Cambia la dificultad del juego, deberás",25,140);
+        text("seleccionar nuevamente tu personaje.",26,160);
+        textFont(fuente,14);
+        text("Jugar de nuevo",145,235);
+        text("Seleccionar otro personaje",100,275);
+        text("Seleccionar otro mapa",120,315);
+        text("Cambiar dificultad",137.5,355);
     }
 }
 function onMouseEvent (Mundo,event) {
