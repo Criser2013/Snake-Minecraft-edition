d4679cc99bcac3bdd8464e0a23a7cbd827fad045
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-30 08:10:19 -0500

    Menú decorado.
    Se decoró el menú agregando un fondo y utilizando la fuente subida con anterioridad.

--- script-coding/dificil/menu_mapas(dificil).js
+++ script-coding/dificil/menu_mapas(dificil).js
@@ -2,6 +2,8 @@ let Mundo = {}
 let mapa1 = null;
 let mapa2 = null;
 let mapa3 = null;
+let fuente = null;
+let fondo = null;
 /*
 Contrato: preload variable -> image
 image = Carga una imagen.
@@ -16,105 +18,120 @@ function preload () {
     mapa1 = loadImage("images/overworld_menu.png");
     mapa2 = loadImage("images/nether_menu.png");
     mapa3 = loadImage("images/end_menu.png");
+    fuente = loadFont("minecraft.otf");
+    fondo = loadImage("images/fondo_mapas.png");
 }
 function setup () {
     createCanvas(400,400);
+    background(fondo);
     Mundo = {boton:2}
 }
 function drawGame () {
     //Esta condición es la encargada de dibujar el menú cuando el "botón 2" se encuentra seleccionado.
     if (Mundo.boton==2) {
-        rect(27.5,277.5,85,25);
-        textFont('Arial',14);
-        text("Selecciona el mapa:",138,95);
+        textFont(fuente,18);
+        text("Selecciona el mapa:",115,110);
         //Esta pare dibuja los marcos que están por fuera de las imagenes de los personajes.
         rect(20,140,100,100);
         rect(150,140,100,100);
         rect(280,140,100,100);
         //Esta parte dibuja los rectangulos de los botones.
-        rect(30,280,80,20);
-        rect(160,280,80,20);
-        rect(290,280,80,20);
-        rect(10,30,50,20);
+        fill(109,109,109);
+        stroke(255);
+        rect(27,280,87,20);
+        stroke(1);
+        rect(157,280,87,20);
+        rect(285,280,87,20);
+        rect(10,30,55,20);
+        fill(255);
         //Esta parte se encarga de dibujar los textos en pantalla.
-        textFont("Arial",14);
-        text("Overworld",39,260);
-        text("Nether",180,260);
-        text("End",317,260);
-        text("Seleccionar",33,295);
-        text("Seleccionar",163,295);
-        text("Seleccionar",293,295);
-        text("Volver",15,45);
+        textFont(fuente,14);
+        text("Overworld",35,260);
+        text("Nether",178,260);
+        text("End",318,260);
+        text("Seleccionar",30,295);
+        text("Seleccionar",160,295);
+        text("Seleccionar",288,295);
+        text("Volver",16,45);
         //Estas 3 lineas son las encargadas de mostrar las imagenes ya cargadas con la función "preload ()" en pantalla.
         image(mapa1,20,140,100,100);
         image(mapa2,150,140,100,100);
         image(mapa3,280,140,100,100);
     }
     else if (Mundo.boton==3) {
-        rect(157.5,277.5,85,25);
-        textFont('Arial',14);
-        text("Selecciona el mapa",138,95);
+        textFont(fuente,18);
+        text("Selecciona el mapa:",115,110);
         rect(20,140,100,100);
         rect(150,140,100,100);
         rect(280,140,100,100);
-        rect(30,280,80,20);
-        rect(160,280,80,20);
-        rect(290,280,80,20);
-        rect(10,30,50,20);
-        textFont("Arial",14);
-        text("Overworld",39,260);
-        text("Nether",180,260);
-        text("End",317,260);
-        text("Seleccionar",33,295);
-        text("Seleccionar",163,295);
-        text("Seleccionar",293,295);
-        text("Volver",15,45);
+        fill(109,109,109);
+        stroke(255);
+        rect(157,280,87,20);
+        stroke(1);
+        rect(27,280,87,20);
+        rect(285,280,87,20);
+        rect(10,30,55,20);
+        fill(255);
+        textFont(fuente,14);
+        text("Overworld",35,260);
+        text("Nether",178,260);
+        text("End",318,260);
+        text("Seleccionar",30,295);
+        text("Seleccionar",160,295);
+        text("Seleccionar",288,295);
+        text("Volver",16,45);
         image(mapa1,20,140,100,100);
         image(mapa2,150,140,100,100);
         image(mapa3,280,140,100,100);
     }
     else if (Mundo.boton==4) {
-        rect(287.5,277.5,85,25);
-        textFont('Arial',14);
-        text("Selecciona el mapa:",138,95);
+        textFont(fuente,18);
+        text("Selecciona el mapa:",115,110);
         rect(20,140,100,100);
         rect(150,140,100,100);
         rect(280,140,100,100);
-        rect(30,280,80,20);
-        rect(160,280,80,20);
-        rect(290,280,80,20);
-        rect(10,30,50,20);
-        textFont("Arial",14);
-        text("Overworld",39,260);
-        text("Nether",180,260);
-        text("End",317,260);
-        text("Seleccionar",33,295);
-        text("Seleccionar",163,295);
-        text("Seleccionar",293,295);
-        text("Volver",15,45);
+        fill(109,109,109);
+        stroke(255);
+        rect(285,280,87,20);
+        stroke(1);
+        rect(27,280,87,20);
+        rect(157,280,87,20);
+        rect(10,30,55,20);
+        fill(255);
+        textFont(fuente,14);
+        text("Overworld",35,260);
+        text("Nether",178,260);
+        text("End",318,260);
+        text("Seleccionar",30,295);
+        text("Seleccionar",160,295);
+        text("Seleccionar",288,295);
+        text("Volver",16,45);
         image(mapa1,20,140,100,100);
         image(mapa2,150,140,100,100);
         image(mapa3,280,140,100,100);
     }
     else if (Mundo.boton==1) {
-        rect(7.5,27.5,55,25);
-        textFont('Arial',14);
-        text("Selecciona el mapa:",138,95);
+        textFont(fuente,18);
+        text("Selecciona el mapa:",115,110);
         rect(20,140,100,100);
         rect(150,140,100,100);
         rect(280,140,100,100);
-        rect(30,280,80,20);
-        rect(160,280,80,20);
-        rect(290,280,80,20);
-        rect(10,30,50,20);
-        textFont("Arial",14);
-        text("Overworld",39,260);
-        text("Nether",180,260);
-        text("End",317,260);
-        text("Seleccionar",33,295);
-        text("Seleccionar",163,295);
-        text("Seleccionar",293,295);
-        text("Volver",15,45);
+        fill(109,109,109);
+        stroke(255);
+        rect(10,30,55,20);
+        stroke(1);
+        rect(27,280,87,20);
+        rect(157,280,87,20);
+        rect(285,280,87,20);
+        fill(255);
+        textFont(fuente,14);
+        text("Overworld",35,260);
+        text("Nether",178,260);
+        text("End",318,260);
+        text("Seleccionar",30,295);
+        text("Seleccionar",160,295);
+        text("Seleccionar",288,295);
+        text("Volver",16,45);
         image(mapa1,20,140,100,100);
         image(mapa2,150,140,100,100);
         image(mapa3,280,140,100,100);
