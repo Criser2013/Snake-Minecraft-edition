bd0e3efb7e9157b3e8c570adb4c2aed4759b166e
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-06-30 08:09:20 -0500

    Menú decorado.
    Se decoró el menú agregando un fondo y utilizando la fuente subida con anterioridad.

--- script-coding/creditos.js
+++ script-coding/creditos.js
@@ -1,30 +1,50 @@
 let Mundo = {}
+let fondo = null;
+let fuente = null;
+/*
+Contrato: preload variable -> FoI
+FoI = Font or image.
+Image = Imagen dentro de los archivos del juego.
+Font = Fuente tipográfica no incluida en CSS ni P5.JS.
+variable = variable local (let)
+Proposito: Carga la fuente estilo Minecraft que utiliza este menú del juego y la imagen de fondo.
+Prototipo: preload () {}
+Ejemplos: preload (fuente,loadImage("minecraft.otf")) -> fuente = loadFont("minecraft.otf") // La fuente se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
+          preload (fondo,loadImage("images/m2.png")) -> fondo = loadImage("images/m2.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
+*/
+function preload () {
+    fuente = loadFont("minecraft.otf");
+    fondo = loadImage("images/fondo_dificultad.png");
+}
 function setup () {
     createCanvas(400,400);
+    background(fondo);
 }
 function drawGame (Mundo) {
-    //Dibuja un rectangulo alrededor de un botón indicando que está seleccionado.
-    rect(172,357,56,26);
     //Dibuja el botón.
-    rect(175,360,50,20);
+    stroke(255);
+    fill(109,109,109);
+    rect(177.5,360,55,20);
+    stroke(1);
+    fill(255);
     //Escribe los textos en pantalla.
-    textFont("Arial",24);
-    text("Créditos",157,30);
-    textFont("Arial",18);
-    text("Beta-testers",154,60);
-    text("Ideas",178,185);
-    text("Programador",152,310);
-    textFont("Arial",14);
+    textFont(fuente,24);
+    text("Equipo de desarrollo",75,30);
+    textFont(fuente,18);
+    text("Beta-testers",142,60);
+    text("Ideas",177,185);
+    text("Programador",142,310);
+    textFont(fuente,14);
     text("- Scott.",175,85);
     text("- Ryder.",172.5,110);
-    text("- Branth.",172.5,135);
-    text("- Dayes (ocasionalmente).",115,160);
+    text("- Branth.",169,135);
+    text("- Dayes (ocasionalmente).",109,160);
     text("- Luisa.",175,210);
     text("- Scott.",175,235);
-    text("- Branth.",172.5,260);
-    text("- Cristian.",170,285);
-    text("- Cristian.",170,335);
-    text("Volver",181,375);
+    text("- Branth.",169,260);
+    text("- Cristian.",167,285);
+    text("- Cristian.",167,335);
+    text("Volver",183,375);
 }
 function onMouseEvent (Mundo,event) {
     return update(Mundo,{});
