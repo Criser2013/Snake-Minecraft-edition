d56ce2419541982836887b337758908f20d2dda4
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-07-23 16:47:03 -0500

    Implementación sonidos de enemigos.
    Se implementaron sonidos al spawnear los enemigos y se arreglaron algunos detalles en los sonidos de comer y morir.

--- script-coding/facil/snakeMovimiento-script(porky-facil-nether).js
+++ script-coding/facil/snakeMovimiento-script(porky-facil-nether).js
@@ -85,7 +85,7 @@ function setup() {
   frameRate(7.5);
   createCanvas(400, 400);
   background(mapa);
-  Mundo = {snake: [{x:3,y:1}, {x:2,y:1}, {x:1,y:1 }],dir:{x:1,y:0},food:{x:foodposx(),y:foodposy()},score:0,colision:false,trampas:{x:foodposx(),y:foodposy(),estado:false},contador:0,obstaculos:{movil:{x:foodposx(),y:foodposy()},estatico:{x:foodposx(),y:foodposy()},respawn:false},sonidos:{muerte: new buzz.sound("audio/muerte",{formats:["mp3"],volume: 40,preload:true}),comer:new buzz.sound("audio/comiendo",{formats:["mp3"],volume: 40,preload:true})}}
+  Mundo = {snake: [{x:3,y:1}, {x:2,y:1}, {x:1,y:1 }],dir:{x:1,y:0},food:{x:foodposx(),y:foodposy()},score:0,colision:false,trampas:{x:foodposx(),y:foodposy(),estado:false},contador:0,obstaculos:{movil:{x:foodposx(),y:foodposy()},estatico:{x:foodposx(),y:foodposy()},respawn:false},sonidos:{muerte: new buzz.sound("audio/muerte",{formats:["mp3"],volume: 40,preload:true}),comer:new buzz.sound("audio/comiendo",{formats:["mp3"],volume: 40,preload:true})},enemigos:{primero:new buzz.sound("audio/cerdo_zombie",{formats:["mp3"],volume: 40,preload:true}),segundo:new buzz.sound("audio/esqueleto",{formats:["mp3"],volume: 40,preload:true}),tercero:new buzz.sound("audio/esqueleto",{formats:["mp3"],volume: 40,preload:true}),cuarto:new buzz.sound("audio/wither",{formats:["mp3"],volume: 40,preload:true})},reproductor:true}
 }
 // Dibuja algo en el canvas. Aqui se pone todo lo que quieras pintar.
 function drawGame(Mundo){
@@ -167,16 +167,40 @@ function drawObstaclesm (obstaculo) {
 }
 function drawObstaclesS (obstaculo) {
   if (Mundo.score>=10&&Mundo.score<20) {
-    image(obstaculoS2,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    if (Mundo.reproductor==true&&Mundo.colision==false) {
+      Mundo.enemigos.primero.play();
+      image(obstaculoS2,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
+    else {
+      image(obstaculoS2,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
   }
   else if (Mundo.score>=20&&Mundo.score<30) {
-    image(obstaculoS3,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    if (Mundo.reproductor==true&&Mundo.colision==false) {
+      Mundo.enemigos.segundo.play();
+      image(obstaculoS3,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
+    else {
+      image(obstaculoS3,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
   }
   else if (Mundo.score>=30&&Mundo.score<50) {
-    image(obstaculoS1,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    if (Mundo.reproductor==true&&Mundo.colision==false) {
+      Mundo.enemigos.tercero.play();
+      image(obstaculoS1,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
+    else {      
+      image(obstaculoS1,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
   }
   else if (Mundo.score>=50) {
-    image(obstaculoS,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    if (Mundo.reproductor==true&&Mundo.colision==false) {
+      Mundo.enemigos.cuarto.play();
+      image(obstaculoS,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
+    else {      
+      image(obstaculoS,obstaculo.x*dx,obstaculo.y*dy,dx,dy);
+    }
   }
 }
 /*
@@ -458,10 +482,20 @@ function colisionCabeza (lista) {
     return false;
   }
   else if ((first(rest(lista)).x==cabeza.x)&&(first(rest(lista)).y==cabeza.y)) {
+    Mundo.enemigos.primero.stop();
+    Mundo.enemigos.segundo.stop();
+    Mundo.enemigos.tercero.stop();
+    Mundo.enemigos.cuarto.stop();
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.muerte.play();
     return true;
   }
   else if ((first(lista).x==first(rest(lista)).x)&&(first(lista).y==first(rest(lista)).y)) {
+    Mundo.enemigos.primero.stop();
+    Mundo.enemigos.segundo.stop();
+    Mundo.enemigos.tercero.stop();
+    Mundo.enemigos.cuarto.stop();
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.muerte.play();
     return true;
   }
@@ -571,23 +605,49 @@ function colisionparedes () {
   }
   //Estas 3 condiciones determinan si hubo colisión con una de las trampas estaticas que spawnean de forma aleatoria a partir de los 10 puntos.
   else if (((((first(Mundo.snake).x+1==Mundo.obstaculos.estatico.x)&&(Mundo.dir.x==1))&&(first(Mundo.snake).y==Mundo.obstaculos.estatico.y))||(((first(Mundo.snake).x-1==Mundo.obstaculos.estatico.x)&&(Mundo.dir.x==-1)))&&(first(Mundo.snake).y==Mundo.obstaculos.estatico.y))&&Mundo.score>=10) {
+    Mundo.enemigos.primero.stop();
+    Mundo.enemigos.segundo.stop();
+    Mundo.enemigos.tercero.stop();
+    Mundo.enemigos.cuarto.stop();
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.muerte.play();
     return true;
   }
   else if (((((first(Mundo.snake).y+1==Mundo.obstaculos.estatico.y)&&(Mundo.dir.y==1))&&(first(Mundo.snake).x==Mundo.obstaculos.estatico.x))||(((first(Mundo.snake).y-1==Mundo.obstaculos.estatico.y)&&(Mundo.dir.y==-1)))&&(first(Mundo.snake).x==Mundo.obstaculos.estatico.x))&&Mundo.score>=10) {
+    Mundo.enemigos.primero.stop();
+    Mundo.enemigos.segundo.stop();
+    Mundo.enemigos.tercero.stop();
+    Mundo.enemigos.cuarto.stop();
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.muerte.play();
     return true;
   }
+  //Estas 4 condiciones evaluan si hay un obstaculo estatico en el lateral opuesto al que se encuentra la serpiente realizando el traslado.
   else if (first(Mundo.snake).x<=-1&&Mundo.obstaculos.estatico.x==19) {
     if (Mundo.obstaculos.estatico.y==first(Mundo.snake).y) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.y==first(Mundo.snake).y+1)&&Mundo.dir.y==1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.y==first(Mundo.snake).y-1)&&Mundo.dir.y==-1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -597,14 +657,29 @@ function colisionparedes () {
   }
   else if (first(Mundo.snake).y<=-1&&Mundo.obstaculos.estatico.y==19) {
     if (Mundo.obstaculos.estatico.x==first(Mundo.snake).x) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.x==first(Mundo.snake).x+1)&&Mundo.dir.x==1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.x==first(Mundo.snake).x-1)&&Mundo.dir.x==-1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -614,14 +689,29 @@ function colisionparedes () {
   }
   else if (first(Mundo.snake).x>=20&&Mundo.obstaculos.estatico.x==0) {
     if (Mundo.obstaculos.estatico.y==first(Mundo.snake).y) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.y==first(Mundo.snake).y+1)&&Mundo.dir.y==1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.x==first(Mundo.snake).x+1)&&Mundo.dir.x==1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -631,14 +721,29 @@ function colisionparedes () {
   }
   else if (first(Mundo.snake).y>=20&&Mundo.obstaculos.estatico.y==0) {
     if (Mundo.obstaculos.estatico.x==first(Mundo.snake).x) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.x==first(Mundo.snake).x+1)&&Mundo.dir.x==1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
     else if ((Mundo.obstaculos.estatico.x==first(Mundo.snake).x-1)&&Mundo.dir.x==-1) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -648,6 +753,11 @@ function colisionparedes () {
   }
   else if ((first(Mundo.snake).x==Mundo.obstaculos.movil.x)&&(first(Mundo.snake).y==Mundo.obstaculos.movil.y)&&Mundo.score>=10) {
     if (Mundo.trampas.estado==true) {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -656,6 +766,11 @@ function colisionparedes () {
       return false;
     }
     else {
+      Mundo.enemigos.primero.stop();
+      Mundo.enemigos.segundo.stop();
+      Mundo.enemigos.tercero.stop();
+      Mundo.enemigos.cuarto.stop();
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.muerte.play();
       return true;
     }
@@ -776,16 +891,19 @@ function onTic(Mundo){
   //Esta condicion se ejecuta cuando la serpiente toma la comida y la comida se encuentra en la misma posicion que un powerup de velocidad, esta condicion activa la suma del score y activa el efecto de aumento de velocidad.
   else if ((Mundo.food.x==Mundo.trampas.x&&Mundo.food.y==Mundo.trampas.y)&&((first(Mundo.snake).x==Mundo.food.x)&&(first(Mundo.snake).y==Mundo.food.y))&&Mundo.score>=5) {
     frameRate(fpscheat()+5);
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.comer.play();
     return update(Mundo,{snake:moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:verificadorComida(Mundo.snake,foodposx(),foodposy()),score:Mundo.score+1,trampas:{estado:true},contador:0})
   }
   //Esta condición determina si hubo colisión entre la cabeza del snake y la comida.
   else if ((first(Mundo.snake).x==Mundo.food.x)&&(first(Mundo.snake).y==Mundo.food.y)) {
     if (Mundo.score>=5) {
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.comer.play();
       return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:verificadorComida(Mundo.snake,foodposx(),foodposy()),score:Mundo.score+1,contador:Mundo.contador+1});
     }
     else {
+      Mundo.sonidos.comer.stop();
       Mundo.sonidos.comer.play();
       return update(Mundo,{snake: moveSnake(crecimiento(Mundo.snake),Mundo.dir),food:verificadorComida(Mundo.snake,foodposx(),foodposy()),score:Mundo.score+1});
     }
@@ -793,31 +911,32 @@ function onTic(Mundo){
    //Esta condicion determina si hubo colisión entre la cabeza del snake y una trampa, aparte de habilitar el efecto de aumento de velocidad, aumentando los FPS del juego 5.
    else if (((first(Mundo.snake).x==Mundo.trampas.x)&&(first(Mundo.snake).y==Mundo.trampas.y))&&Mundo.score>=5) {
     frameRate(fpscheat()+5);
+    Mundo.sonidos.comer.stop();
     Mundo.sonidos.comer.play();
     return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:{estado:true},contador:0})
   }
    //Esta condición es la encargada de hacer desaparecer el efecto de aumento de velocidad en la serpiente luego de 20 segundos 
   else if (Mundo.contador>=80&&Mundo.trampas.estado==true) {
     frameRate(fpscheat())
-    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:verificadorTrampas(Mundo.snake,cheatposx(),cheatposy()),obstaculos:{movil:{x:Mundo.obstaculos.movil.x,y:Mundo.obstaculos.movil.y},estatico:{x:Mundo.obstaculos.estatico.x,y:Mundo.obstaculos.estatico.y},respawn:false},contador:0})
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:verificadorTrampas(Mundo.snake,cheatposx(),cheatposy()),obstaculos:{movil:{x:Mundo.obstaculos.movil.x,y:Mundo.obstaculos.movil.y},estatico:{x:Mundo.obstaculos.estatico.x,y:Mundo.obstaculos.estatico.y},respawn:false},contador:0,reproductor:false})
   }
-  //Esta condición determina si la cabeza del snake se encuentra en los límites laterales del mapa, para asi realizar el cambio de posición.
+  //Estas condiciones determinan si la cabeza del snake se encuentra en los límites laterales del mapa, para asi realizar el cambio de posición.
   else if ((first(Mundo.snake).x>=20||first(Mundo.snake).x<=-1)||((first(Mundo.snake).y<=-1)||(first(Mundo.snake).y>=20))) {
     return update(Mundo,{snake:moveSnake(traslacion(Mundo.snake),Mundo.dir)})
   }
   //Esta condición hace de cronometro cuando se toma una de las trampas.
   else if (Mundo.trampas.estado==true&&Mundo.contador<80) {
-    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1})
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1,reproductor:false})
   }
   //Esta condición actua como cronometro para spawnear una nueva trampa siempre y cuando el usuario no la haya cogido.
   else if ((Mundo.contador>=0&&Mundo.contador<40)&&(Mundo.trampas.estado==false&&Mundo.score>=5)) {
     frameRate(fpscheat());
-    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1})
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),contador:Mundo.contador+1,reproductor:false})
   }
    //Esta condicion spawnea una nueva trampa y nuevos obstaculos cada que la condicion anterior alcanza un valor de 40 en el parametro "contador" del mundo.
   else if (Mundo.contador>=40&&Mundo.trampas.estado==false) {
     frameRate(fpscheat());
-    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:verificadorTrampas(Mundo.snake,cheatposx(),cheatposy()),obstaculos:{movil:verificadorObstaculosM(Mundo.snake,obsposx(),obsposy()),estatico:verificadorObstaculosE(Mundo.snake,cheatposx(),cheatposy()),respawn:true},contador:0})
+    return update(Mundo,{snake: moveSnake(Mundo.snake,Mundo.dir),trampas:verificadorTrampas(Mundo.snake,cheatposx(),cheatposy()),obstaculos:{movil:verificadorObstaculosM(Mundo.snake,obsposx(),obsposy()),estatico:verificadorObstaculosE(Mundo.snake,cheatposx(),cheatposy()),respawn:true},contador:0,reproductor:true})
   }
   else {
     //Actualiza la posición del snake usando la función "moveSnake".
