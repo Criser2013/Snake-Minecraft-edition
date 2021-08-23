be2cc4997d8c70087aa69e10856a91332b15ebde
Criser2013 <84862634+Criser2013@users.noreply.github.com>
2021-08-23 16:56:39 -0500

    Problemas de carga solucionados.
    Se solucionó el problema de carga que hacía que la página cargase primero que el script del juego.

--- script-coding/medio/menu_mapas(medio).js
+++ script-coding/medio/menu_mapas(medio).js
@@ -4,25 +4,13 @@ let mapa2 = null;
 let mapa3 = null;
 let fuente = null;
 let fondo = null;
-/*
-Contrato: preload variable -> image
-image = Carga una imagen.
-variable = variable local (let)
-Proposito: Carga las imagenes (también funciona con archivos JSON) que se muestran en los menus, antes del "setup ()" para acerlerar 
-la carga de estos elementos en pantalla.
-Prototipo: preload () {}
-Ejemplos: preload (flecha1,loadImage("images/m1.png")) -> flecha1 = loadImage("images/m1.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
-          preload (flecha2,loadImage("images/m2.png")) -> flecha2 = loadImage("images/m2.png") // La imagen se carga en la memoria de forma permanente aún hasta cuando se llame a la funcion setup ().
-*/
-function preload () {
+function setup () {
+    createCanvas(400,400);
     mapa1 = loadImage("images/overworld_menu.png");
     mapa2 = loadImage("images/nether_menu.png");
     mapa3 = loadImage("images/end_menu.png");
     fuente = loadFont("minecraft.otf");
     fondo = loadImage("images/fondo_mapas.png");
-}
-function setup () {
-    createCanvas(400,400);
     background(fondo);
     Mundo = {boton:2,sonido:new buzz.sound("audio/seleccion",{formats:["mp3"],volume: 50,preload:true})}
 }
